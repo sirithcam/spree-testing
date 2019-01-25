fe2368ac04d0564caf831374a6b8a4b04523bd7a
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2019-01-25 15:52:57 +0100

    add screenshots on failures

--- spec/helpers/capybara_helper.rb
+++ spec/helpers/capybara_helper.rb
@@ -3,3 +3,9 @@
 Capybara.default_driver = ENV['BROWSER'].to_sym
 
 Capybara.app_host = ENV['APP_HOST']
+
+Capybara.save_path = 'logs/screenshots'
+
+Capybara::Screenshot.register_driver(ENV['BROWSER'].to_sym) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
