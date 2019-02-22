e7bf1e107e850128fdc15db9a06e94c9f0fede77
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2019-02-22 09:18:07 +0100

    add rubocop-rspec gem

--- spec/features/test_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.feature 'test' do
-  scenario 'test' do
-    visit 'https://app.keepabl.com'
-    fill_in 'user_email', with: 'bang@wp.pl'
-    click_button 'Sign in'
-    expect(page).to_not have_content 'Invalid Email or password.'
-  end
-end
