269a31d3d4368797471e69035894d8c37c19a2ee
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2019-03-08 12:49:53 +0100

    add wait_for method

--- spec/helpers/main_helper.rb
+++ spec/helpers/main_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module MainHelper
+  def wait_for(options = {})
+    default_options = {
+      error: nil,
+      seconds: 5
+    }.merge(options)
+
+    Selenium::WebDriver::Wait.new(timeout: options[:seconds]).until { yield }
+  rescue Selenium::WebDriver::Error::TimeOutError
+    options[:error].nil? ? false : raise(error)
+  end 
 end
