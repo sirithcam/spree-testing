3a534988445f41f05ec27551ba5502a620c358eb
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2019-03-08 12:54:53 +0100

    add 3 additional methods to MainHelper

--- spec/helpers/main_helper.rb
+++ spec/helpers/main_helper.rb
@@ -7,8 +7,20 @@ module MainHelper
       seconds: 5
     }.merge(options)
 
-    Selenium::WebDriver::Wait.new(timeout: options[:seconds]).until { yield }
+    Selenium::WebDriver::Wait.new(timeout: default_options[:seconds]).until { yield }
   rescue Selenium::WebDriver::Error::TimeOutError
-    options[:error].nil? ? false : raise(error)
-  end 
+    default_options[:error].nil? ? false : raise(error)
+  end
+
+  def scroll_to_bottom
+    page.execute_script 'window.scrollBy(0,10000)'
+  end
+
+  def scroll_to_top
+    page.execute_script 'window.scrollTo(0,0)'
+  end
+
+  def scroll_to(element)
+    Capybara.current_session.driver.browser.execute_script('arguments[0].scrollIntoView(true);', element.native)
+  end
 end
