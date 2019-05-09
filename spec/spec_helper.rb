e45c57b9aa6c3ed4b564705c30aca09c10cb2f7f
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2019-05-09 14:31:31 +0200

    change browser resolution and add skipping pending tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ Dir['./spec/shared_examples/**/*.rb'].each { |file| require file }
 RSpec.configure do |config|
   config.include MainHelper
 
+  config.filter_run_excluding :block => nil
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -23,9 +25,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-  config.before { Capybara.page.driver.browser.manage.window.resize_to(1240, 1400) if ENV['BROWSER'] == 'safari' }
+  config.before { Capybara.page.driver.browser.manage.window.resize_to(1240, 1400) }
 
   config.append_after { Capybara.reset_sessions! }
 end
